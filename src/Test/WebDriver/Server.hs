fd69aae546d906ae3e0842a29eedeb5ecc6fd386
Joel <me@joelt.io>
2014-02-09 20:34:46 -0800

    test passing

--- src/Test/WebDriver/Server.hs
+++ src/Test/WebDriver/Server.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE CPP #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
 module Test.WebDriver.Server (
@@ -9,7 +10,12 @@ module Test.WebDriver.Server (
     ServerSettings(..)
 ) where
 
+import Control.Concurrent
+import Control.Exception
+import Control.Monad
 import Network
+import System.IO
+import System.Process
 import qualified Test.WebDriver.Server.Download as Download
 
 -- | Where the server can be found. 'Remote' means @selenium-server@ won't be
@@ -30,9 +36,34 @@ defaultSettings :: ServerSettings
 defaultSettings = ServerSettings Local Nothing Nothing
 
 -- | Execute the given 'IO' action with a selenium server running.
-withServer :: ServerSettings -> IO () -> IO ()
-withServer ss act = do
-    jarfile <- case jarLocation ss of
-                   Nothing -> Download.downloadJar (serverVersion ss)
-                   Just f -> return f
-    error $ show jarfile
+withServer :: ServerSettings -> IO a -> IO a
+withServer ss act = case serverLocation ss of
+    Remote{} -> error "not handled yet"
+    Local -> do
+        jarfile <- case jarLocation ss of
+                       Nothing -> Download.downloadJar (serverVersion ss)
+                       Just f -> return f
+        blackHole <- openFile
+#ifdef WINDOWS
+                         "NUL"
+#else
+                         "/dev/null"
+#endif
+                         AppendMode
+        bracket (do (_,_,_,ch) <- createProcess
+                        (proc "java" ["-jar", jarfile, "-port", "4444"])
+                            { std_in = UseHandle blackHole
+                            , std_out = UseHandle blackHole
+                            , std_err = CreatePipe }
+                    waitForServer
+                    return ch)
+                (\h -> terminateProcess h >> hClose blackHole)
+                (const act)
+
+waitForServer :: IO ()
+waitForServer = void $ go 0 where
+    go x | x >= (30 :: Int) = error "Timed out after 30 seconds waiting for selenium server."
+    go n = do
+        threadDelay 1000000
+        connectTo "127.0.0.1" (PortNumber 4444)
+            `catch` (\(_ :: IOException) -> go (n + 1))
--- tests/main.hs
+++ tests/main.hs
@@ -1,9 +1,23 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main where
+
+import qualified Data.Text as T
 import Test.Hspec
+import Test.WebDriver
 import Test.WebDriver.Server
+import Test.WebDriver.Commands.Wait
 
 main :: IO ()
 main = hspec $
     describe "The server" $
         it "runs" $ do
-            v <- withServer defaultSettings (return ())
-            v `shouldBe` ()
+            v <- withServer defaultSettings $
+                runSession defaultSession defaultCaps $ do
+                    openPage "http://www.baidu.com"
+                    searchBox <- findElem (ByName "wd")
+                    sendKeys "Cheese!" searchBox
+                    submit searchBox
+                    setImplicitWait 50
+                    waitUntil 2000 getTitle
+            T.take 7 v `shouldBe` "Cheese!"
